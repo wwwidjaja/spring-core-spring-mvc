4164a5927fb81c15f20182ca3ae33b609d069b51
William Widjaja <wwwidjaja@yahoo.com>
2018-07-15 14:51:03 -0400

    testing CI build

--- src/main/java/guru/springframework/SpringmvcApplication.java
+++ src/main/java/guru/springframework/SpringmvcApplication.java
@@ -8,5 +8,7 @@ public class SpringmvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringmvcApplication.class, args);
+        
+        // this is a whitespace change
     }
 }
